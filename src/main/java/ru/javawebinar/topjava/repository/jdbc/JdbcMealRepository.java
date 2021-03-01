ae10ab1a24ec4da04a7f8f2dacea096e5b5801f8
Nemchinov Sergey <googoodemon@gmail.com>
2021-03-01 15:15:40 +0700

    fix spring tests with profiles

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
@@ -7,6 +8,7 @@ import ru.javawebinar.topjava.repository.MealRepository;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Profile("jdbc")
 @Repository
 public class JdbcMealRepository implements MealRepository {
 
