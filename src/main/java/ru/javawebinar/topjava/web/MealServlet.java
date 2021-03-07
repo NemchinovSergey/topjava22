139dc6885b2721e4d21ead6c2115f70532bf23b8
Nemchinov Sergey <googoodemon@gmail.com>
2021-03-07 17:56:39 +0700

    HW03 4 dirty hack to set spring profile for repository beans

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -27,6 +27,7 @@ public class MealServlet extends HttpServlet {
 
     @Override
     public void init() {
+        System.setProperty("spring.profiles.active", "jdbc"); // dirty hack to set spring profile for repository beans
         springContext = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/spring-db.xml");
         mealController = springContext.getBean(MealRestController.class);
     }
